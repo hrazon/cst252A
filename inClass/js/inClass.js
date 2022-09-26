e861b86678b493edc6225126a411b23bfd6b70b0
hrazon <89794586+hrazon@users.noreply.github.com>
2022-09-26 15:28:59 -0700

    lab 7

---
+++ inClass/js/inClass.js
@@ -0,0 +1,27 @@
+console.log ('hello');
+//MAKE A VARIABLE THAT HAS AN OBJECT IN IT
+//THE OBJECT IS KEEPING TRACK OF 3 PROPERTIES/ITEMS
+///THE NAME OF THE PLAYER: HEAVEN
+///THE COLOR THEIR TUNIC: GREEN
+///A NUMBER THAT SAYS HOW MUCH HELATH THEY HAVE LEFT
+
+
+
+
+//MAKE A FUNCTION
+//THE FUNCTION HAS 1 PARAMATER CALLED COLOR
+//NAME THE FUNCTION WHATEVER YYOU WANT
+//IT SHOULD SET THE PLAYER'S TUNIC COLOR TO WHATEVER THE COLOR WE WANT
+
+
+
+//Player needs a backpack
+//A backpack is a collection of things that are represented by words. Here's what could be in the backpack: 'health potion', 'sword'
+
+//IN THE PLAYER OBJECT, ADD A PROPERTY CALLED BACKPACK
+//MAKE IT SO THAT THE BACKPACK STARTS OUT HAVING HEALTH POTION & A SWORD
+
+
+//I want to make a function that lets me put something in the BACKPACK
+///MAKE A FUNCTION THAT HAS ONE PARAMETER (item)
+//It makes that item and adds it  to the backpack
