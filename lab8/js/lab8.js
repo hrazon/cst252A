62f3d9dbc17d2fdecdc67e5066748d4fb9f3aaa9
hrazon <89794586+hrazon@users.noreply.github.com>
2022-10-04 19:26:38 -0700

    lab 8

---
+++ lab8/js/lab8.js
@@ -0,0 +1,12 @@
+//**
+//* Author: Heaven Razon
+//* Created: 10/04/2022
+//* (c) Copyright by Blub Corp.
+
+// function to add text to given element
+function main() {
+    // get html element by id
+    outputElement = document.getElementById("output");
+    // change contents of element
+    outputElement.innerHTML = "Yo, 'sup?";
+}
