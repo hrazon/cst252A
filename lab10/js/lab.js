2aee71b203c60c4c6d40643789e10bd5bcaa9bae
hrazon <89794586+hrazon@users.noreply.github.com>
2022-10-10 18:43:53 -0700

    lab 10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,16 +1,43 @@
-let showPanel = (e) => {
-  console.log(e);
+document.addEventListener("DOMContentLoaded" , () => {
+  let itemBox = document.querySelector ('.itembox');
+  let showPanel = (e) => {
+    console.log(e);
 
-
-  let panel = document.querySelector('#infopanel');
-  console.log(panel);
+  let panel = document.querySelector("#infopanel");
+  console.log(infopanel);
   panel.style.display = "block";
 
-let p = document.querySelector("#courseName");
-console.log(courseName);
-courseName.textContent = "CST 252";
-}
+  let p = document.querySelector("#courseName");
+  console.log(courseName);
+  courseName.textContent = "CST 252: Scripting for Multimedia";
 
-document.addEventListener("DOMContentLoaded", () => {
+  let description = document.querySelector('#courseDescription');
+  console.log(description);
+  description.textContent = "A course about learning JavaScript.";
 
-});
+  document.querySelector("#courseimage").src= "images/cst252.jpg";
+  }
+  itemBox.addEventListener('click',showPanel);
+
+  let theBorder = document.querySelector('.itembox');
+
+  let showBorder = () => {
+    theBorder.style.border = "thick solid";
+
+  }
+
+  let hideBorder = () => {
+    theBorder.style.border = 'none';
+  }
+
+  theBorder.addEventListener('mouseover', showBorder);
+  theBorder.addEventListener('mouseout', hideBorder);
+
+  let exitButton = document.querySelector('#exitbutton');
+  let infoPanel = document.querySelector('#infoPanel');
+  let closePanel = () => {
+    infoPanel.style.display = 'none';
+
+  }
+  exitButton.addEventListener('click',closePanel);
+})
